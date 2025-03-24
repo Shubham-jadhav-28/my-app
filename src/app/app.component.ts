78dfd93215847faa6381615028ee1490abfa9e4a
Shubham-jadhav-28 <shubham.jadhav@waiin.com>
2025-03-24 19:21:20 +0530

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <h1>Angular 4 App</h1>`
+  // imports: [RouterOutlet],
+  // templateUrl: './app.component.html',
+  // styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-app';
